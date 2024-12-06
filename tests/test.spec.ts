f2644031c4ce8458eb517b13f1e388392265a29a
Roy test <roypolocarrillo@gmail.com>
2024-12-06 10:30:03 -0500

    correccion en el test

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -3,6 +3,6 @@ import { test } from '@playwright/test';
 test.describe('hola ', () => {
   test('hola', async ({ page }) => {
     await page.goto('http://localhost:5173/');
-    await page.getByRole('button', { name: 'Calcular1' }).click();
+    await page.getByRole('button', { name: 'Calcular' }).click();
   })
 });
